1b9591f3a7ab7e65e35ee85da2cf332834294d9f
Pascal Bies <pascal.bies@k-lens.de>
2018-03-31 01:52:11 +0200

    fixed init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ source env/bin/activate
 pip install -r requirements.txt
 ./canweb/manage.py migrate
 
-echo "done. run `./canweb/manage.py` and open your browser at localhost:8000/view/song/list/"
+echo 'done. run `./canweb/manage.py runserver` and open your browser at localhost:8000/view/song/list/'
